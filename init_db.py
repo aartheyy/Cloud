ee313c15b7b1a3f576c88e36f788a6e2858c53f8
Somasundaram aarthi <64091886+aartheyy@users.noreply.github.com>
2025-04-04 21:57:22 +0800

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -1,7 +1,6 @@
-#from app import app
-#from db import db
-#import models  # this will import your Task model
+from app import app
+from db import db
+import models  # This will import the Task model
 
-# This will create all tables defined in models (like Task)
-#with app.app_context():
-    #db.create_all()
+with app.app_context():
+    db.create_all()
