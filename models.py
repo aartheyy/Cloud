10c437caaa122aff34ae13eaf7b78d696c9ff520
Somasundaram aarthi <64091886+aartheyy@users.noreply.github.com>
2025-04-04 21:27:57 +0800

    Update models.py

--- models.py
+++ models.py
@@ -4,3 +4,6 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     completed = db.Column(db.Boolean, default=False)
+
+    def __repr__(self):
+        return f'<Task {self.id} - {self.title}>'
