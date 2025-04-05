a756b9fe95871679948d49d2c05c81ff72047231
Somasundaram aarthi <64091886+aartheyy@users.noreply.github.com>
2025-04-05 20:28:51 +0800

    Update models.py

--- models.py
+++ models.py
@@ -5,11 +5,13 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
+    tasks = db.relationship('Task', backref='owner', lazy=True)
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     completed = db.Column(db.Boolean, default=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
         return f'<Task {self.title}>'
