491b62643f5866adc3bcf5e1baebf4b3a4c35b45
Somasundaram aarthi <64091886+aartheyy@users.noreply.github.com>
2025-04-05 18:16:15 +0800

    added user func

--- models.py
+++ models.py
@@ -1,4 +1,10 @@
 from db import db
+from flask_login import UserMixin
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
