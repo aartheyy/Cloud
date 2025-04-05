38f1212c33b282dcac4ad2fad244c67bae8ba447
Somasundaram aarthi <64091886+aartheyy@users.noreply.github.com>
2025-04-05 19:08:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,28 @@
+# app.py
+
 from flask import Flask, render_template
-from models import Task
-from db import db
-import os
-import routes  # make sure this is after Flask
 from flask_login import LoginManager
-from models import User
 from flask_bcrypt import Bcrypt
+
+from models import Task, User
+from db import db
 
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'supersecretkey'  # required for session cookies
 
 db.init_app(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = 'login'  # redirect for @login_required
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.before_first_request
 def create_tables():
@@ -29,17 +35,15 @@ def create_tables():
         ]
         db.session.add_all(sample_tasks)
         db.session.commit()
-        
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-    
+
 @app.route("/")
 def index():
-    return render_template("index.html")  # <-- must be after `render_template` is imported
+    return render_template("index.html")
 
-routes.register_routes(app)  # <-- must be called after `app` is created
+from auth_routes import register_auth_routes
+register_auth_routes(app)
 
 if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 8000))
     app.run(host="0.0.0.0", port=port)
