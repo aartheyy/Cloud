30c9576b04c05c03e264bc7a6fba359d9ed62f25
Somasundaram aarthi <64091886+aartheyy@users.noreply.github.com>
2025-04-05 16:53:44 +0800

    include some db items

--- app.py
+++ app.py
@@ -13,6 +13,16 @@ db.init_app(app)
 @app.before_first_request
 def create_tables():
     db.create_all()
+
+    # Insert only if DB is empty
+    if not Task.query.first():
+        sample_tasks = [
+            Task(title="Learn Azure"),
+            Task(title="Build To-Do List App"),
+            Task(title="Push to GitHub & Deploy", completed=True)
+        ]
+        db.session.add_all(sample_tasks)
+        db.session.commit()
     
 @app.route('/')
 def home():
