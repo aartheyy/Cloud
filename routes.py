609d488f99af2f6d5b159585d5401c7e75ab71bc
Somasundaram aarthi <64091886+aartheyy@users.noreply.github.com>
2025-04-05 18:43:58 +0800

    Update routes.py

--- routes.py
+++ routes.py
@@ -2,6 +2,7 @@ from flask import request, jsonify
 from models import Task
 from db import db
 from flask_login import login_user, login_required, logout_user
+from app import bcrypt
 
 def register_routes(app):
     @app.route('/tasks', methods=['GET'])
