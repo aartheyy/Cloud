12d9be7768056e496a3029d0dcb0a7e8304775b6
Somasundaram aarthi <64091886+aartheyy@users.noreply.github.com>
2025-04-05 18:19:09 +0800

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,6 +1,9 @@
 from flask import request, jsonify
 from models import Task
 from db import db
+from flask_login import login_user, login_required, logout_user
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
 
 def register_routes(app):
     @app.route('/tasks', methods=['GET'])
@@ -33,3 +36,27 @@ def register_routes(app):
         db.session.delete(task)
         db.session.commit()
         return jsonify({'message': 'Task deleted'})
+
+    @app.route('/login', methods=['POST'])
+    def login():
+        data = request.get_json()
+        user = User.query.filter_by(username=data['username']).first()
+        if user and bcrypt.check_password_hash(user.password, data['password']):
+            login_user(user)
+            return jsonify({'message': 'Logged in'})
+        return jsonify({'error': 'Invalid credentials'}), 401
+    
+    @app.route('/register', methods=['POST'])
+    def register():
+        data = request.get_json()
+        hashed_pw = bcrypt.generate_password_hash(data['password']).decode('utf-8')
+        new_user = User(username=data['username'], password=hashed_pw)
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify({'message': 'User registered'})
+    
+    @app.route('/logout')
+    @login_required
+    def logout():
+        logout_user()
+        return jsonify({'message': 'Logged out'})
